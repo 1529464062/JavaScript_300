65dc79ec2179a82a7aeb61b28adee1fd67f2e796
1529464062 <1529464062@qq.com>
2015-11-09 19:25:38 +0800

    实例，完毕，未做通用化开发。

---
+++ 005_若干秒后不操作自动关闭窗口/JS/index.js
@@ -0,0 +1,27 @@
+﻿//定义一个是否关闭的全局变量
+window.onload=function(){
+	PageHelp.Init();
+}
+function clickBody(){		
+	document.getElementById("time").innerHTML="10";
+}
+/*
+setInterval(function(){
+	if(parseInt(willClose)<=0){
+		window.close();
+	}
+},10000);
+*/
+setInterval(function(){
+	console.log(document.getElementById("time").innerHTML);
+	document.getElementById("time").innerHTML=parseInt(document.getElementById("time").innerHTML)-1;
+	if(parseInt(document.getElementById("time").innerHTML)==0){
+		window.close();
+	};	
+},1000);
+var PageHelp={
+	Init:function(){
+		document.getElementById("time").innerHTML="10";
+		document.getElementsByTagName("body")[0].addEventListener("click",clickBody);
+	}
+}
