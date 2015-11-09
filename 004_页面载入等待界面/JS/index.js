201f6ff02d4dcbf34cd330c39e17a9f013dee222
1529464062 <1529464062@qq.com>
2015-11-09 18:41:26 +0800

    状态：完成。

---
+++ 004_页面载入等待界面/JS/index.js
@@ -0,0 +1,15 @@
+﻿window.onload=function(){
+	document.getElementById("submit").addEventListener("click",submitForm);
+	pageHelp.Init();
+}
+function submitForm(){
+	var doing=document.getElementById("doing");
+	doing.style.display="block";
+	self.location.reload();
+	//location.href="http://www.baidu.com";
+}
+var pageHelp={
+	Init:function(){
+		alert("页面加载成功");
+	}
+}
