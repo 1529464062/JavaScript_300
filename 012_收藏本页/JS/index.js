2f70bdbf91041b9e53a5c28161bd2a364c4759ee
1529464062 <1529464062@qq.com>
2015-11-10 14:59:23 +0800

    012_收藏本页，失败。

---
+++ 012_收藏本页/JS/index.js
@@ -0,0 +1,11 @@
+﻿window.onload=function(){
+	document.getElementById("addFav").href="javascript:void(0)";
+	document.getElementById("addFav").addEventListener("click",addFav);
+}
+function addFav(){
+	if(window.external.AddFavorite){
+		window.external.AddFavorite('./单击加入收藏夹.html','javascript300实例');
+	}else{
+		window.sidebar.addPanel('javascript300实例','http://www.baidu.com','');
+	}
+}
