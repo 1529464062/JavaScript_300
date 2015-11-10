aa1b970c2a1d3a04c3f766fddb097a3321d213ec
1529464062 <1529464062@qq.com>
2015-11-10 15:42:37 +0800

    014_避免浏览器使用缓存，执行正确，未做通用化开发，未做集成性开发。

---
+++ 014_避免浏览器使用缓存加载页面/JS/index.js
@@ -0,0 +1,25 @@
+﻿function getLinkHref(){
+	var links=document.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++){
+		var link=links[i];
+		var href=link.href;
+		if(href.indexOf('?')>-1){
+			href+='&time='+new Date().getTime();
+			console.log(href);
+		}else{
+			href+='?time='+new Date().getTime();
+			console.log(href);
+		}
+		link.href=href;
+	}
+}
+/*
+window.onload=function(){
+	getLinkHref();
+}
+*/
+document.onreadystatechange=function(){
+	if(document.readyState=='complete'){
+		getLinkHref();
+	}	
+}
