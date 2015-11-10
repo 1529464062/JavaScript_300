8efc3024cdcd53e52afcfab90e66aeb0f116fef4
1529464062 <1529464062@qq.com>
2015-11-10 14:47:05 +0800

    011_设为首页，不成功，原因不清楚，貌似是没有那个属性。

---
+++ 011_设为首页/JS/index.js
@@ -0,0 +1,21 @@
+﻿window.onload=function(){
+	document.getElementById("setHome").href="javascript:void(0)";
+	document.getElementById("setHome").addEventListener("click",setMyHomePage);
+}
+function setMyHomePage(){
+	//判断是否为ie浏览器
+	if(document.all){
+		document.body.style.behavior='url(#default#homepage)';
+		document.body.setHomePage('http://www.myhomepage.com');
+		console.log("设置成功");
+	}else{
+		if(window.sidebar){
+			//通过Firefox特有的组件设置首页地址
+			var thePrefs=Components.classes['@mozilla.org/preference-service;1'].getservice(components,interface.nsIPrefBranch);
+			thePrefs.setCharPref('browser.startup.homepage','http://www.myhomgpage.com');
+			console.log("设置成功");
+		}else{
+			console.log("设置不成功");
+		}
+	}
+}
